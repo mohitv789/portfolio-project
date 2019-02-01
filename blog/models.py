bcbc6890a7345c384dd1bc5fb05fe7b4ace87f57
mohitv789 <mohitv15@gmail.com>
2019-02-01 20:33:47 +0530

    My rescue Operation

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+# Create a BLOG models/
+# title , publication date , body , image
+class Blog(models.Model):
+    title = models.CharField(max_length = 255)
+    publication_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to = 'images/')
+# ADD blog app to setting
+# migration
+#add to admin
+    def summary(self):
+        return self.body[:100]
+    def pub_date_pretty(self):
+        return self.publication_date.strftime('%b %e %Y')
+    def __str__(self):
+        return self.title
---
+++ myprojectenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ubuntu/Desktop/Python/Django/Django_2.0/Projects/portfolio_final/myprojectenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
